6f4f616c28c12f05671cd95297f7229ba6c15e93
danielvago99 <danielvago.osc@gmail.com>
2026-01-21 17:19:54 +0100

    Add leaderboard update service

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import http from 'http';
 import { app } from './app';
 import { env } from './config/env';
+import { updateLeaderboards } from './services/leaderboard';
 import { createWsServer } from './ws/server';
 import { logger } from './utils/logger';
 
@@ -9,6 +10,14 @@ const server = http.createServer(app);
 
 // Attach WebSocket server
 createWsServer(server);
+
+const LEADERBOARD_INTERVAL = 5 * 60 * 1000;
+
+void updateLeaderboards();
+
+setInterval(() => {
+  void updateLeaderboards();
+}, LEADERBOARD_INTERVAL);
 
 server.listen(env.PORT, () => {
   logger.info(`Backend server listening on http://localhost:${env.PORT}`);
